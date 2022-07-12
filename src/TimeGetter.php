1e6926c6fb65cc918bce9fc3673f04c605ece9b8
Pipo leong <pipoleong@genwhy.com.my>
2022-07-12 15:13:25 +0800

    init time

---
+++ src/TimeGetter.php
@@ -0,0 +1,16 @@
+<?php
+
+class TimeGetter
+{
+
+    /**
+     * get Current Time.
+     *
+     * @return int
+     */
+    public static function getCurrentTime()
+    {
+        return time();
+    }
+
+}
